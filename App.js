4f793e04347102945c250f3599ea2266c3cf704f
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2024-02-02 15:32:27 +0300

    create routes foltder

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Routes } from './routes'
+
+const App = () => {
+  return (
+    <View>
+      <Routes />
+    </View>
+  )
+}
+
+export default App
