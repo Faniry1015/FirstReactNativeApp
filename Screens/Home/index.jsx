ebc2056a8ea87f0d5cf099abeb7a2a3fc1bce438
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2024-02-11 07:07:51 +0300

    create my home screen

---
+++ Screens/Home/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>This is my home page</Text>
+    </View>
+  )
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home } from '../Screens/Home';
 
 export const Routes = () => {
+
+  
+const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <NavigationContainer>
+          <Stack.Navigator initialRouteName='home' screenOptions={{headerShown: false}}> 
+        <Stack.Screen name="home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
