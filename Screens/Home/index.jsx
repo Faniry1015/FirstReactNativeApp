2338950675b405ac160a2d1bf6988ee4270a5031
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2024-02-13 16:56:58 +0300

    app big error

--- Screens/Home/index.jsx
+++ Screens/Home/index.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 import FaniryImg from '../../assets/images/faniry.jpg'
 import { dashboardStyles } from './style.jsx'
 import { FakeActivity } from '../../fakeData/fakeActivity.jsx'
-import AgriSvg from '../../assets/images/svg/planting.svg'
-import Svg, { Circle } from 'react-native-svg';
+// import AgriSvg from '../../assets/images/svg/planting.svg'
+// import Svg, { Circle } from 'react-native-svg';
 
 export const Home = () => {
     return (
@@ -26,9 +26,9 @@ export const Home = () => {
                 renderItem={({ item }) => {
                     return (
                         <TouchableOpacity style={dashboardStyles.scrollableListItem}>
-                            <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+                            {/* <Svg height="50%" width="50%" viewBox="0 0 100 100" >
                                 <Circle cx="50" cy="50" r="50" stroke="purple" strokeWidth=".5" fill="violet" />
-                            </Svg>
+                            </Svg> */}
                             <Text style={dashboardStyles.mainText}>{item.mainText}</Text>
                             <Text style={dashboardStyles.subText}>{item.subText}</Text>
                         </TouchableOpacity>
