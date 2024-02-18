5b7a901e0f58959a57234a796855f0f6b4a55e3c
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2024-02-18 08:00:08 +0300

    add style to conversation

--- Components/Message/index.jsx
+++ Components/Message/index.jsx
@@ -7,8 +7,11 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 
 export const Message = ({item}) => {
+    const isMine = () => {
+        return item.user.id === 1
+    }
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: isMine() ? '#dcf8c5': 'white', alignSelf: isMine() ? 'flex-end' : 'flex-start'}]}>
       <Text>{item.message}</Text>
       <Text>{dayjs(item.createAt).fromNow(true)}</Text>
     </View>
--- Components/Message/style.jsx
+++ Components/Message/style.jsx
@@ -6,5 +6,6 @@ export const styles = StyleSheet.create({
         margin: 5,
         padding: 10,
         borderRadius: 5, 
+        maxWidth: '90%'
     }
 })
