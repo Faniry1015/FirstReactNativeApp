9ef8fa77352ae836ac1f3baa788d434375bf0e48
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2024-02-19 06:51:15 +0300

    create fakeSettings

---
+++ fakeData/fakeSettings.jsx
@@ -0,0 +1,30 @@
+export const fakeSettings = [
+    {
+        id: 1,
+        libelle:'Profil',
+    },
+    {
+        id:2,
+        libelle: 'Mes documents',
+    },
+    {
+        id: 3,
+        libelle: 'Mes rendez vous',
+    },
+    {
+        id: 4,
+        libelle: 'Contacts utiles',
+    },
+    {
+        id: 5,
+        libelle: 'Personnalisation',
+    },
+    {
+        id: 6,
+        libelle: 'Paramètres avancés',
+    },
+    {
+        id: 7,
+        libelle: 'Déconnexion',
+    },
+]
